76ddd01e096235c37e256b5a7ff6f1b9b89426dc
Forrest <Forrestmillerj@gmail.com>
2020-06-01 21:21:49 -0500

    server working using gorilla mux

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":3000", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "<h1>Gorilla Mux Router</h1>")
 }
