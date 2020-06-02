8297d25687933ac9f9af0ec95b870f628a5a6934
Forrest <Forrestmillerj@gmail.com>
2020-06-01 21:48:26 -0500

    grabbing params from url

--- main.go
+++ main.go
@@ -10,10 +10,18 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", hello).Methods("GET")
+	r.HandleFunc("/{name}", name).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "<h1>Gorilla Mux Router</h1>")
+}
+
+func name(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	name := params["name"]
+	fmt.Fprintf(w, string(name))
+
 }
